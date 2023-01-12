2bc280b98d3abf5f70e8711806539532ded926f8
rfarrell961 <rff961@gmail.com>
2023-01-11 22:26:58 -0500

    Getting rid of more console logs

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -41,8 +41,6 @@ router.route('/register').post(async (req,res) =>
 router.route('/login').post(async (req, res) => {
 
     const { email, password } = req.body;
-    console.log(email);
-    console.log(password);
     if (!email || !password)
     {
         return res.status(500).send({Error: "Please enter all fields!"});
