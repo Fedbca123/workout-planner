728876a6236da9f9b57c81e7ab36a02997f519cc
Alice Margo Zhang <alicezhang32092@knights.ucf.edu>
2023-01-17 14:17:02 -0500

    Updating friends

--- frontend/screens/friends.js
+++ frontend/screens/friends.js
@@ -1,12 +1,46 @@
-import { StyleSheet, Button, Text, Image, View, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Button, ListItem, Text, Image, View, SafeAreaView, TextInput, Card, Icon } from 'react-native';
 import React from 'react';
 
-const Friends = () => {
+const users = [
+    {
+       name: 'brynn',
+       avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+    },
+]
+
+export default function Friends(props) {
     return  (
-        <View style={{flex : 1}}>
-            <Text>This is the friends page</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.Title}>Friends</Text>
+            <Text style={styles.Heading}>Workout friends</Text>
+            
+            {/* Will put flat list here and turn them into cards */}
+            
+            <Text style={styles.Heading}>Add a friend</Text>
+        </SafeAreaView>
     )
 }
 
-export default Friends; 
+const styles = StyleSheet.create({
+    Title:{
+        fontFamily: 'HelveticaNeue-Bold',
+        color: '#2B2B2B',
+        fontSize: 24,
+        textAlign: 'left',
+        paddingLeft: 20,
+    },
+    Heading:{
+        fontFamily: 'HelveticaNeue',
+        color: '#2B2B2B',
+        fontSize: 18,
+        textAlign: 'left',
+        paddingLeft: 20,
+    },
+    Card:{
+        borderRadius: 15,
+        backgroundColor: '#2B2B2B',
+    },
+    cardContent:{
+        
+    }
+});
