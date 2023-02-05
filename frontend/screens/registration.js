88aa4fc7af11c7ae76a61a37c574c449bb48bc4d
Nestor Rubio <nestordrubio9@gmail.com>
2023-02-05 10:57:23 -0500

    added registration to context workflow

--- frontend/screens/registration.js
+++ frontend/screens/registration.js
@@ -1,14 +1,16 @@
 import { StyleSheet, Button, Text, Image, View, SafeAreaView, TextInput, Pressable } from 'react-native';
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {ScrollView, KeyboardAvoidingView} from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import axios from 'axios';
 import config from '../../config';
+import {useGlobalState} from '../../GlobalState.js';
+
 const baserUrl = config.API_URL + config.PORT + '/';
 const allGood = true;
 
-class Register extends React.Component{
-  constructor(props) {
+//class Register extends React.Component{
+  /*constructor(props) {
     super(props)
     this.state = {
       firstName: '',
@@ -24,121 +26,179 @@ class Register extends React.Component{
     this.passwordRef = React.createRef();
     this.passwordConfirmRef = React.createRef();
     this.registerHandler = this.registerHandler.bind(this);
-  }
+  }*/
+export default function Register(props) {
+  // defining state
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConf, setPasswordConf] = useState('');
+  const [error, setError] = useState('');
+  const [globalState, updateGlobalState] = useGlobalState();
 
-  registerHandler () {
-    const emptyFirstname = this.state.firstName === '';
-    const emptyLastname = this.state.lastName === '';
-    const emptyEmail = this.state.email === '';
-    const emptyPassword = this.state.password === '';
-    const emptyPasswordConf = this.state.passwordConfirmation === '';
+  // defining refs
+  const firstNameRef = useRef(0);
+  const lastNameRef = useRef(0);
+  const emailRef = useRef(0);
+  const passwordRef = useRef(0);
+  const passwordConfRef = useRef(0);
+
+  const registerHandler = () => {
+    //const emptyFirstname = this.state.firstName === '';
+    //const emptyLastname = this.state.lastName === '';
+    //const emptyEmail = this.state.email === '';
+    //const emptyPassword = this.state.password === '';
+    //const emptyPasswordConf = this.state.passwordConfirmation === '';
+
+    const emptyFirstname = firstName === '';
+    const emptyLastname = lastName === '';
+    const emptyEmail = email === '';
+    const emptyPassword = password === '';
+    const emptyPasswordConf = passwordConf === '';
 
     // if all empty fields, one message is sufficient
     if(emptyFirstname && emptyLastname && emptyEmail && emptyPassword && emptyPasswordConf){
-      this.firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
-      this.lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
-      this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
-      this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
-      this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
 
-      this.setState({error: "Please enter all fields!"});
+      firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
+
+      //this.setState({error: "Please enter all fields!"});
+      setError("Please enter all fields!");
       return;
     }
 
-    let error = '';
+    let tempError = '';
 
     // -----first name -----//
     if (emptyFirstname){
-      this.firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += '- Please provide a first name!\n';
+      //this.firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      firstNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += '- Please provide a first name!\n';
     }
-    else this.firstNameRef.current.setNativeProps({style: styles.inputstyle});
+    else firstNameRef.current.setNativeProps({style: styles.inputstyle});
+    //else this.firstNameRef.current.setNativeProps({style: styles.inputstyle});
 
     // -----last name -----//
     if (emptyLastname) {
-      this.lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += "- Please provide a last name!\n";
+      //this.lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      lastNameRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += "- Please provide a last name!\n";
     }
-    else this.lastNameRef.current.setNativeProps({style: styles.inputstyle});
+    else lastNameRef.current.setNativeProps({style: styles.inputstyle});
+    //else this.lastNameRef.current.setNativeProps({style: styles.inputstyle});
 
     // -------email----------//
     if (emptyEmail){
-      this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += "- Please provide an email address!\n";
+      //this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += "- Please provide an email address!\n";
     }
     // make sure email field matches a regex
-    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.state.email)){
-      this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += '- Email is not in proper format!\n'
+    else if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email/*this.state.email*/)){
+      //this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += '- Email is not in proper format!\n'
     }
-    else this.emailRef.current.setNativeProps({style: styles.inputstyle});
+    else emailRef.current.setNativeProps({style: styles.inputstyle});
+    //else this.emailRef.current.setNativeProps({style: styles.inputstyle});
 
     //----password and confirmation------//
-    this.passwordRef.current.setNativeProps({style: styles.inputstyle});
-    this.passwordConfirmRef.current.setNativeProps({style: styles.inputstyle});
+    //this.passwordRef.current.setNativeProps({style: styles.inputstyle});
+    //this.passwordConfirmRef.current.setNativeProps({style: styles.inputstyle});
+    passwordRef.current.setNativeProps({style: styles.inputstyle});
+    passwordConfRef.current.setNativeProps({style: styles.inputstyle});
 
     if(emptyPassword && emptyPasswordConf){
-      this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
-      this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += '- Please provide both a password and password confirmation!\n';
+      //this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+      passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += '- Please provide both a password and password confirmation!\n';
     }
     else if (emptyPassword) {
-      this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
-      error += "- Please provide a password!\n";
+      //this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+      tempError += "- Please provide a password!\n";
     }
     else if (emptyPasswordConf) 
     {
-        this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
-        error += '- Please enter the confirmed password!\n';
+        //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+        passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
+        tempError += '- Please enter the confirmed password!\n';
     }
-    else if (this.state.password !== this.state.passwordConfirmation)
+    else if (password !== passwordConf/*this.state.password !== this.state.passwordConfirmation*/)
     {
-        this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
-        error += '- Passwords do not match!\n';
+        //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+        passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
+        tempError += '- Passwords do not match!\n';
     }
-    else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(this.state.password))
+    else if(!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password/*this.state.password*/))
     {
-        this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
-        this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
-        error += '- Passwords must be at least 8 characters and have at least one uppercase letter, one lowercase letter, and one number.';
+        //this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+        passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+        //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+        passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
+        tempError += '- Passwords must be at least 8 characters and have at least one uppercase letter, one lowercase letter, and one number.';
     }
 
     // if every test passes, error is none
-    if(error != ''){
-      this.setState({error: error});
+    if(tempError != ''){
+      //this.setState({error: error});
+      setError(tempError)
       return;
     }else{
-      this.setState({error: ''});
+      //this.setState({error: ''});
+      setError('');
     }
 
     axios.post(baserUrl + 'users/register', {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        password: this.state.password
+        //firstName: this.state.firstName,
+        //lastName: this.state.lastName,
+        //email: this.state.email,
+        //password: this.state.password
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
     })
     .then((response) => {
         if (response.status == 200)
         {
-            this.setState({error: ''});
-            this.props.navigation.navigate("home", {user: response.data});
+            //this.setState({error: ''});
+            setError('');
+            //this.props.navigation.navigate("home", {user: response.data});
+            updateGlobalState("user", response.data);
+
+            props.navigation.navigate("home", {user: response.data});
         }
     })
     .catch((e) => {
-        if (e.response) this.setState({error: e.response.data.Error});
+        if (e.response) setError(e.response.data.Error)//this.setState({error: e.response.data.Error});
         if (e.response.status == 501)
         {
-            this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
-            this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+            //this.passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+            passwordRef.current.setNativeProps({style: styles.inputerrorstyle});
+            //this.passwordConfirmRef.current.setNativeProps({style: styles.inputerrorstyle});
+            passwordConfRef.current.setNativeProps({style: styles.inputerrorstyle});
         }
         else if(e.response.status == 502)
         {
-            this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+            //this.emailRef.current.setNativeProps({style: styles.inputerrorstyle});
+            emailRef.current.setNativeProps({style: styles.inputerrorstyle});
         }
     });
   }
 
-  render(){
+  //render(){
     return  (
       <KeyboardAwareScrollView 
         extraHeight={100}
@@ -150,26 +210,26 @@ class Register extends React.Component{
             <TextInput style={styles.inputstyle} 
             placeholder="John"
             returnKeyType="next"
-            ref={this.firstNameRef}
+            ref={firstNameRef}
             keyboardType="next"
-            onSubmitEditing={() => {this.lastNameRef.current.focus();}}
-            onChangeText={(text)=>this.setState({firstName:text})}/>
+            onSubmitEditing={() => {lastNameRef.current.focus();}}
+            onChangeText={(text)=> setFirstName(text)}/>
             
             <Text style={styles.text}>Last Name</Text>
             <TextInput style={styles.inputstyle} 
             placeholder="Smith"
             returnKeyType="next"
-            ref={this.lastNameRef}
-            onSubmitEditing={() => {this.emailRef.current.focus();}}
-            onChangeText={(text)=>this.setState({lastName:text})}/>
+            ref={lastNameRef}
+            onSubmitEditing={() => {emailRef.current.focus();}}
+            onChangeText={(text)=>setLastName(text)}/>
 
             <Text style={styles.text}>Email Address</Text>
             <TextInput style={styles.inputstyle} 
             placeholder="user@server.com"
             returnKeyType="next"
-            ref={this.emailRef}
-            onSubmitEditing={() => {this.passwordRef.current.focus();}}
-            onChangeText={(text)=>this.setState({email:text})}/>
+            ref={emailRef}
+            onSubmitEditing={() => {passwordRef.current.focus();}}
+            onChangeText={(text)=> setEmail(text)}/>
 
             <Text style={styles.text}>Password</Text>
             <TextInput style={styles.inputstyle} 
@@ -177,9 +237,9 @@ class Register extends React.Component{
             secureTextEntry={true}
             returnKeyType="next"
             autoCapitalize='none'
-            ref={this.passwordRef}
-            onSubmitEditing={() => {this.passwordConfirmRef.current.focus();}}
-            onChangeText={(text)=>this.setState({password:text})}/>
+            ref={passwordRef}
+            onSubmitEditing={() => {passwordConfRef.current.focus();}}
+            onChangeText={(text)=> setPassword(text)}/>
 
             <Text style={styles.text}>Confirm Password</Text>
             <TextInput style={styles.inputstyle} 
@@ -187,21 +247,21 @@ class Register extends React.Component{
             secureTextEntry={true}
             returnKeyType="done"
             autoCapitalize='none'
-            ref={this.passwordConfirmRef}
-            onChangeText={(text)=>this.setState({passwordConfirmation:text})}/>
+            ref={passwordConfRef}
+            onChangeText={(text)=> setPasswordConf(text)}/>
           </View>
 
           <View style={{
             width:'90%',
             alignSelf: 'center'
             }}>
-            <Text style={styles.error}>{this.state.error}</Text>
+            <Text style={styles.error}>{error}</Text>
           </View>
           
           <View style={styles.buttoncontainer}>
             <Pressable
               style={styles.button}
-              onPress={()=>{this.registerHandler()}}>
+              onPress={()=>{registerHandler()}}>
               <Text style={styles.buttontext}>Register</Text>
             </Pressable>
             <View style ={{margin:5}}>
@@ -210,14 +270,14 @@ class Register extends React.Component{
                 color="#C4C4C4"
                 accessibilityLabel="Back to Login"
                 onPress={() => {
-                  this.props.navigation.navigate("login")
+                  props.navigation.navigate("login")
               }}/>
             </View>
           </View>
         </View>
       </KeyboardAwareScrollView>
     )
-  }
+  //}
 }
 
 const styles = StyleSheet.create({
@@ -315,4 +375,4 @@ const styles = StyleSheet.create({
 }
 });
 
-export default Register; 
+//export default Register; 
