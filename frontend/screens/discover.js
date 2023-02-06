c1dc903326d924cb4c2c519bfa302aa1484bcf7e
stevenhorn17 <stevenhorn02@gmail.com>
2023-02-06 15:01:13 -0500

    Changes to Discover
    it better

--- frontend/screens/discover.js
+++ frontend/screens/discover.js
@@ -1,25 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function DiscoverPage(props) {
   return (
-    
-    <View style={styles.container}>
-        <View>
-            <Text>Discover New Exercises!</Text>
-        </View>
-        <View>
-            <Text>Discover New Workouts!</Text>
-        </View>
-            {/* <StatusBar style= "auto" />*/}
+    <View>
+      <View style={styles.discoveryPageHeader}>
+        <Text style={styles.discoverTitle}>Discover</Text>
+        <Text style={styles.discoverSubtitle}>Refresh your fitness knowledge or learn something new</Text>
+            <View style={styles.discoverBttnsCntnr}>
+              <View style={styles.discoverWorkoutsBttnsContainer}>
+                <TouchableOpacity onPress={console.log("Workouts Pressed")}>
+                  <Text style={styles.workoutsBttnText}>Workouts</Text>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.discoverExercisesBttnsContainer}>
+                <TouchableOpacity onPress={console.log("Exercises Pressed")}>
+                  <Text style={styles.exercisesBttnText}>Exercises</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+                {/* <StatusBar style= "auto" />*/}
+            </View>
     </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
+  discoveryPageHeader:{
+    backgroundColor: 'white',
+  },
+  workoutsBttnText:{
+    color: '#12BEF6',
+    fontWeight: 'bold',
+  },
+  exercisesBttnText:{
+    color: '#FA7B34',
+    fontWeight: 'bold',
+  },
+  discoverBttnsCntnr:{
+    alignContent: 'center',
+    flexDirection: 'row',
+  },
+  discoverWorkoutsBttnsContainer:{
+    alignItems: 'center',
+    backgroundColor: '#DCF1FE',
+    margin: 30,
+    padding: 15,
+    borderRadius: '10rem',
+  },
+  discoverExercisesBttnsContainer:{
+    alignItems: 'center',
+    backgroundColor: '#F8E1D2',
+    margin: 30,
+    padding: 15,
+    borderRadius: '10rem',
+  },
+  discoverTitle:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginLeft: 10,
+  },
+  discoverSubtitle:{
+    padding: 10,
+    opacity: .45,
+  },
   container: {
-    height: 500,
-    backgroundColor: '#fff',
+    height: 700,
+    //backgroundColor: '#fff',
     alignItems: 'stretch',
     justifyContent: 'space-evenly',
   },
