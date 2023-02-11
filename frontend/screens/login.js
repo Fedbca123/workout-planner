6382fd65bc3369997e3408a05a5f15a13fdbb77f
Nestor Rubio <nestordrubio9@gmail.com>
2023-02-11 14:35:14 -0500

    removing the tons of comments left behind. Cleaner code and minor modifications to keyboard use flow in login

--- frontend/screens/login.js
+++ frontend/screens/login.js
@@ -17,22 +17,6 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const baseUrl = config.API_URL + config.PORT + '/';
 
 export default function Login(props) {
-//class Login extends React.Component {
-    //constructor (state and such)
-    //constructor(props) {
-    //    super(props)
-    //    this.state = {
-    //        email: '',
-    //        password: '',
-    //        error: '',
-    //    }
-    //    this.passwordRef = React.createRef();
-    //    // bind all functions to class
-    //    this.emailInputHandler = this.emailInputHandler.bind(this);
-    //    this.passwordInputHandler = this.passwordInputHandler.bind(this);
-    //    this.loginHandler = this.loginHandler.bind(this);
-    //}
-
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -41,120 +25,102 @@ export default function Login(props) {
       
     // functions
     const emailInputHandler = (enteredEmail) => {
-        //this.setState({email: enteredEmail});
         setEmail(enteredEmail);
     }
       
     const passwordInputHandler = (enteredPassword) =>{
-        //this.setState({password: enteredPassword});
         setPassword(enteredPassword);
       }
 
     const loginHandler = () => {
         axios.post(baseUrl + "users/login", {
             email: email,
-            password: password  
-          //email: this.state.email,
-            //password: this.state.password
+            password: password
         })
         .then((response) => {
             if (response.status == 200)
             {
-                //this.setState({error: ''});
                 setError('');
-                updateGlobalState("user", response.data)
-                //this.props.navigation.navigate("home",{user: response.data});
+                updateGlobalState("user", response.data);
                 props.navigation.navigate("home");
             }
         })
         .catch((e) => {
-            //if (e.response) this.setState({error: e.response.data.Error});
             if (e.response) setError(e.response.data.Error);
           });
     }
     const backDoorHandler = () => {
         axios.post(baseUrl + "users/login", {
             email: "Test@gmail.com",
-            password: "password"  
-          //email: this.state.email,
-            //password: this.state.password
+            password: "password"
         })
         .then((response) => {
             if (response.status == 200)
             {
-                //this.setState({error: ''});
                 setError('');
                 updateGlobalState("user", response.data)
-                //this.props.navigation.navigate("home",{user: response.data});
                 props.navigation.navigate("home");
             }
         })
         .catch((e) => {
-            //if (e.response) this.setState({error: e.response.data.Error});
             if (e.response) setError(e.response.data.Error);
           });
     }
-    //render() {
     return (
-        <KeyboardAwareScrollView
-          extraHeight={100} contentContainerStyle={styles.container} keyboardShouldPersistTaps="handled" bounces={false}>
-                <Image style={styles.image}
-                    source={require('../../assets/workout.png')} />
-                <View style={styles.textcontainer}>
-                    <Text style = {styles.heading}> Welcome! </Text>
-                    <Text style = {styles.text}> You will have everything you need to reach your personal fitness goals - for free! </Text>
-                </View>
-                <View style={styles.buttoncontainer}>
-                <Button
-                        title="BACKDOOR"
-                        onPress={() => backDoorHandler()}
-                     />
-                    <Text style = {styles.error}> {error} </Text>
-                    <TextInput style={styles.inputstyle} 
-                        placeholder="Email"
-                        returnKeyType="next"
-                        onSubmitEditing={() => {passwordRef.current.focus();}}
-                        blurOnSubmit={false}
-                        keyboardType="email-address"
-                        onChangeText={(text) => emailInputHandler(text)}/>
-
-                    <TextInput style={styles.inputstyle} 
-                        placeholder="Password"
-                        returnKeyType="go"
-                        autoCapitalize='none'
-                        ref={passwordRef}
-                        secureTextEntry
-                        onChangeText={(text) => {passwordInputHandler(text)}}
-                        onSubmitEditing={()=> {
-                            passwordRef.current.blur()
-                            loginHandler()
-                        }}/>
-
-                    <Button
-                        title="Login"
-                        color="#10B9F1"
-                        // expecting line below to turn into authentication or page switching soon enough //
-                        onPress={() => loginHandler()}
-                        // onPress={() => {
-                        //     this.state.error = '';
-                        //     this.props.navigation.navigate("calendar");
-                        //   }}
-                        />
-
-                    <Button 
-                        title="Create an account"
-                        color="#C4C4C4"
-                        accessibilityLabel="Create an account"
-                        onPress={() => {
-                            //this.state.error = '';
-                            setError('');
-                            //this.props.navigation.navigate("registration");
-                            props.navigation.navigate("registration");
-                        }}/>
-                        </View>
-        </KeyboardAwareScrollView>
+      <KeyboardAwareScrollView
+        extraHeight={100}
+        contentContainerStyle={styles.container}
+        keyboardShouldPersistTaps="handled"
+        bounces={false}
+      >
+        <Image style={styles.image}
+          source={require('../../assets/workout.png')} />
+        <View style={styles.textcontainer}>
+          <Text style = {styles.heading}> Welcome! </Text>
+          <Text style = {styles.text}> You will have everything you need to reach your personal fitness goals - for free! </Text>
+        </View>
+          <View style={styles.buttoncontainer}>
+          <Button
+            title="BACKDOOR"
+            onPress={() => backDoorHandler()}
+          />
+          <Text style = {styles.error}> {error} </Text>
+          <TextInput style={styles.inputstyle} 
+            placeholder="Email"
+            returnKeyType="next"
+            onSubmitEditing={() => {passwordRef.current.focus();}}
+            blurOnSubmit={false}
+            keyboardType="email-address"
+            onChangeText={(text) => emailInputHandler(text)}/>
+          <TextInput style={styles.inputstyle} 
+            placeholder="Password"
+            returnKeyType="go"
+            autoCapitalize='none'
+            ref={passwordRef}
+            secureTextEntry
+            onChangeText={(text) => {passwordInputHandler(text)}}
+            onSubmitEditing={()=> {
+                passwordRef.current.blur()
+                loginHandler()
+            }}
+          />
+          <Button
+            title="Login"
+            color="#10B9F1"
+            onPress={() => loginHandler()}
+          />
+          <Button 
+            title="Create an account"
+            color="#C4C4C4"
+            accessibilityLabel="Create an account"
+            onPress={() => {
+              setError('');
+              props.navigation.navigate("registration");
+            }}
+          />
+        </View>
+      </KeyboardAwareScrollView>
     )
-    //}
 }
       
 const styles = StyleSheet.create({
@@ -215,5 +181,3 @@ const styles = StyleSheet.create({
         fontSize: 16,
     }
 });
-      
-//export default Login;
