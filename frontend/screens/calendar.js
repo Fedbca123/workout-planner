07c60e87eeefa774a6db5958979eee524011ab65
ricedrink <alicezhang32092@knights.ucf.edu>
2023-03-06 14:20:12 -0500

    changes to friends and calendar

--- frontend/screens/calendar.js
+++ frontend/screens/calendar.js
@@ -15,15 +15,15 @@ LocaleConfig.defaultLocale = 'en';
 const CalendarScreen = () => {
   const [selectedDate, setSelectedDate] = useState('');
   const [eventList, setEventList] = useState([
-    { date: '2023-02-26', title: 'Running', type: 'Cardio', sets: 3, recurring: true },
-    { date: '2023-02-27', title: 'Yoga', type: 'Flexibility', sets: 1, recurring: false },
-    { date: '2023-02-28', title: 'Weightlifting', type: 'Strength', sets: 5, recurring: true },
-    { date: '2023-02-29', title: 'Pilates', type: 'Core', sets: 2, recurring: false },
+    { date: '2023-03-16', title: 'Running', type: 'Cardio', sets: 3, recurring: true },
+    { date: '2023-03-17', title: 'Yoga', type: 'Flexibility', sets: 1, recurring: false },
+    { date: '2023-03-18', title: 'Weightlifting', type: 'Strength', sets: 5, recurring: true },
+    { date: '2023-03-19', title: 'Pilates', type: 'Core', sets: 2, recurring: false },
     { date: '2023-03-01', title: 'Swimming', type: 'Endurance', sets: 4, recurring: true },
   ]);
 
   const [friendEventList, setFriendEventList] = useState([
-    { date: '2023-02-12', title: 'Soccer', type: 'Cardio', friendName: 'John' },
+    { date: '2023-03-116', title: 'Soccer', type: 'Cardio', friendName: 'John' },
     { date: '2023-02-14', title: 'Basketball', type: 'Cardio', friendName: 'Mary' },
   ]);
 
@@ -47,7 +47,7 @@ const CalendarScreen = () => {
   const renderEvent = ({ item }) => {
     if (item.date === selectedDate) {
       return (
-        <View style={{ padding: 10 }}>
+        <View style={{ padding: 10, backgroundColor: '#24C8FE'}}>
           <Text style={{ fontWeight: 'bold' }}>{item.date}</Text>
           <Text style={{ fontWeight: 'bold' }}>{item.title}</Text>
           <Text>Workouts: {item.type}</Text>
@@ -57,6 +57,21 @@ const CalendarScreen = () => {
     }
     return null;
   };
+
+  const renderFriend = ({ item }) => {
+    if (item.date === selectedDate) {
+      return (
+        <View style={{ padding: 10, backgroundColor: '#grey'}}>
+          <Text style={{ fontWeight: 'bold' }}>{item.date}</Text>
+          <Text style={{ fontWeight: 'bold' }}>{item.title}</Text>
+          <Text>Workouts: {item.type}</Text>
+
+        </View>
+      );
+    }
+    return null;
+  };
+
 
   return (
     <View style={styles.container}>
@@ -85,3 +100,4 @@ const styles = StyleSheet.create({
 });
 
 export default CalendarScreen;
+
