d0e48fb99892262c24bd2041c5d237624ee19f37
ricedrink <alicezhang32092@knights.ucf.edu>
2023-03-16 15:31:18 -0400

    fetchapi works

--- frontend/screens/inbox.js
+++ frontend/screens/inbox.js
@@ -9,26 +9,85 @@ import {
     Alert
 } from "react-native";
 import React, { useState, useRef, useEffect } from "react";
-import { Icon } from 'react-native-elements'
-import { useGlobalState } from "../GlobalState.js";
+import {useGlobalState} from '../GlobalState.js';
 import API_Instance from "../../backend/axios_instance";
-import * as SecureStore from 'expo-secure-store';
-import { AuthContext } from '../AuthProvider';
 
-
-export default function Inbox({ navigation }) 
-{
-    return(
-            <View style={styles.container}>
-                <Text style={{ paddingLeft: 20 }}>Incoming friend requests</Text>
-            </View>
-
+export default function Inbox({ navigation }) {
+    const [filteredFriends, setFilteredFriends] = useState([]);
+    const [globalState, updateGlobalState] = useGlobalState();
+    
+    const fetchFriendRequests = async () => {
+      try {
+        const response = await API_Instance.get(`users/${globalState.user._id}/invites/all`, {
+          headers: {
+            'authorization': `Bearer ${globalState.authToken}`,
+          },
+        });
+  
+        const friendRequests = response.data.friends || [];
+        setFilteredFriends(friendRequests);
+      } catch (error) {
+        console.error(error);
+        if (error.response?.status === 403) {
+          Alert.alert('Failed to authenticate you');
+        }
+      }
+    };
+  
+    useEffect(() => {
+      fetchFriendRequests();
+    }, []);
+  
+    const acceptFriendRequest = (friendId) => {
+      // handle accepting friend request
+    };
+  
+    const declineFriendRequest = (friendId) => {
+      // handle declining friend request
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Text style={{ paddingLeft: 20 }}>Incoming friend requests</Text>
+        {filteredFriends.length > 0 ? (
+          <FlatList
+            data={filteredFriends}
+            keyExtractor={(item) => item._id}
+            renderItem={({ item }) => (
+              <View style={styles.friendRequestContainer}>
+                <Text>{item.username} wants to be your friend!</Text>
+                <View style={styles.friendRequestButtons}>
+                  <TouchableOpacity onPress={() => acceptFriendRequest(item._id)}>
+                    <Text>Accept</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity onPress={() => declineFriendRequest(item._id)}>
+                    <Text>Decline</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )}
+          />
+        ) : (
+          <Text>No friend requests</Text>
+        )}
+      </View>
     );
-}
-
-styles = StyleSheet.create({
+  }
+  
+  const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: 'white',
-    }
-});
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    friendRequestContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 10,
+      borderBottomWidth: 1,
+      borderBottomColor: 'lightgray',
+    },
+    friendRequestButtons: {
+      flexDirection: 'row',
+    },
+  });
