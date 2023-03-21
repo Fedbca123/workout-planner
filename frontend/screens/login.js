22bec2b5c4dab48d75ae0c2d16e7c37774e1aa32
Omar Shalaby <omarshalaby911@knight.ucf.edu>
2023-03-21 16:22:37 -0400

    fixed Robert's backdoor

--- frontend/screens/login.js
+++ frontend/screens/login.js
@@ -104,10 +104,10 @@ export default function Login({navigation}) {
 				</Text>
 			</View>
       
-        <Button
+        {/* <Button
         title="BACKDOOR"
         onPress={() =>
-          backDoorHandler("Test@gmail.com", "password")}/>
+          backDoorHandler("Test@gmail.com", "Password1")}/> */}
 
       {/* <Button
         title="ADMIN BACKDOOR"
