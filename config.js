fa9bc18ac405242500037ef585219042d6f74144
rfarrell961 <rff961@gmail.com>
2023-04-08 11:55:01 -0400

    Checks for if Constanst.expoConfig.extra is null

--- config.js
+++ config.js
@@ -2,9 +2,13 @@ import Constants from "expo-constants";
 
 const { manifest } = Constants;
 
-console.log("Constants", Constants.expoConfig.extra.APP_ENV);
+if (!Constants.expoConfig.extra)
+    console.log("Constants undefined");
+else{
+    console.log("Constants", Constants.expoConfig.extra.APP_ENV);
+}
 
-if (Constants.expoConfig.extra.APP_ENV === 'production')
+if (Constants.expoConfig.extra && Constants.expoConfig.extra.APP_ENV === 'production')
     url = process.env.API_URL;
 else
     url = `http://${manifest.debuggerHost.split(':').shift()}:${process.env.PORT}`;
