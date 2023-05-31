cb737b43d445cea6d774b2c75319b09191d3efe7
Nestor Rubio <nestordrubio9@gmail.com>
2023-05-31 19:06:33 -0400

    added a completedWorkout schema and added object ids to user schema for completedworkouts

---
+++ backend/models/completedWorkout.model.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let {Exercise, exerciseSchema} = require('./exercise.model');
+
+var completedWorkoutSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: false,
+    trim: true,
+    minlength: 2
+  },
+  description: {
+    type: String,
+    required: false,
+    unique: false,
+    trim: true
+  },
+  image: String,
+  imageId: String,
+  exercises: {
+    type: [exerciseSchema],
+    required: false,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: false,
+  },
+  recurrence: {
+    type: Boolean,
+    required: false,
+  },
+  dateOfCompletion: {
+    type: Date,
+    required: false,
+  },
+  tags: {
+    type: [String],
+    required: false
+  },
+  muscleGroups: {
+    type: [String],
+    required: false
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+}, {timestamps: true});
+
+const CompletedWorkout = mongoose.model('CompletedWorkout', completedWorkoutSchema);
+
+module.exports = {CompletedWorkout, completedWorkoutSchema};
